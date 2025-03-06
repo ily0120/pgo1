8084315c89a9379e31d9f951246e06b9c1c05a80
Ilya Lopan <s30042@pjwstk.edu.pl>
2025-03-06 11:04:48 +0100

    zmiana 1

--- pgo1_2/src/Main.java
+++ pgo1_2/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        System.out.println(a+c);
     }
 }
